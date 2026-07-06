f615bf7605569782e6d8ed84b5425bc8a2252b77
Brian <ronggg27825@gmail.com>
2026-07-06 09:58:44 +0800

    feat: update version 0.1.1

--- src/youtube_mcp/_version.py
+++ src/youtube_mcp/_version.py
@@ -18,7 +18,7 @@ version_tuple: tuple[int | str, ...]
 commit_id: str | None
 __commit_id__: str | None
 
-__version__ = version = '0.1.1.dev0+gbb41f14ca.d20260706'
-__version_tuple__ = version_tuple = (0, 1, 1, 'dev0', 'gbb41f14ca.d20260706')
+__version__ = version = '0.1.2.dev1+g78d7bf293.d20260706'
+__version_tuple__ = version_tuple = (0, 1, 2, 'dev1', 'g78d7bf293.d20260706')
 
 __commit_id__ = commit_id = None
