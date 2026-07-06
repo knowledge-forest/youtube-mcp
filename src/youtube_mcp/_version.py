d55090e7a7aa6057fbc8ec366945e27034b5db2c
Brian <ronggg27825@gmail.com>
2026-07-06 09:59:56 +0800

    chore: bump into v0.1.2

--- src/youtube_mcp/_version.py
+++ src/youtube_mcp/_version.py
@@ -18,7 +18,7 @@ version_tuple: tuple[int | str, ...]
 commit_id: str | None
 __commit_id__: str | None
 
-__version__ = version = '0.1.2.dev1+g78d7bf293.d20260706'
-__version_tuple__ = version_tuple = (0, 1, 2, 'dev1', 'g78d7bf293.d20260706')
+__version__ = version = '0.1.2'
+__version_tuple__ = version_tuple = (0, 1, 2)
 
 __commit_id__ = commit_id = None
